757a03c6da650d77c2079a0362011c084964e6db
hongsamhc2 <hongsamgcp@gmail.com>
2023-06-01 17:53:32 +0900

    1. 게시물 등 createdAt 자동 기입을 위해 EnableJpaAuditing 추가 2. 게시물 User 정보를 UserInfoResponseDto 타입으로 변경 3. UserInfoResponseDto 에 email 추사 4. PostListResponseDto 에 content 추가 5. addResourceHandlers 업로드 디렉토리가 없을 경우 생성

--- src/main/java/ium/pethub/PethubApplication.java
+++ src/main/java/ium/pethub/PethubApplication.java
@@ -4,8 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+
+/**
+ * 수정자: 조훈창
+ * 수정일: 2023-06-01
+ * 1. 게시물 등 createdAt 자동 기입을 위해 EnableJpaAuditing 추가
+ */
 @EnableJpaAuditing
+@SpringBootApplication
 public class PethubApplication {
 
 	public static void main(String[] args) {
--- src/main/java/ium/pethub/dto/user/reponse/UserInfoResponseDto.java
+++ src/main/java/ium/pethub/dto/user/reponse/UserInfoResponseDto.java
@@ -3,15 +3,24 @@ package ium.pethub.dto.user.reponse;
 import ium.pethub.domain.entity.User;
 import lombok.Getter;
 
+/**
+ * 수정자: 조훈창
+ * 수정일 : 2023-06-01
+ * 수정 내용: email 추가
+ * 
+ */
+
 @Getter
 public class UserInfoResponseDto {
 
     private String nickname;
     private String userImage;
+    private String email;
 
 
     public UserInfoResponseDto(User user){
         this.nickname = user.getNickname();
         this.userImage = user.getUserImage();
+        this.email = user.getEmail();
     }
 }
